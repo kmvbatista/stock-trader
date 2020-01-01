b64724d5647cb9eb6e39489765553c2f43db1a9b
Kennedy Batista <kendy2022@hotmail.com>
2019-12-31 22:57:23 -0300

    portfolio

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,48 @@
+const state = {
+  funds: 1000,
+  stocks: []
+};
+
+const mutations = {
+  BUY_STOCK(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(stock => stock.id === stockId);
+    if (record) {
+      record.quantity += quantity;
+    } else {
+      state.stocks.push({ id: stockId, quantity });
+    }
+    state.funds -= stockPrice * quantity;
+  },
+  SELL_STOCK(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(stock => stock.id === stockId);
+    if (record.quantity > quantity) {
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(state.stocks.indexOf(record), 1);
+    }
+    state.funds -= stockPrice * quantity;
+  }
+};
+
+const actions = {
+  sellStock({ commit }, order) {
+    commit('SELL_STOCK', order);
+  }
+};
+
+const getters = {
+  stockPortfolio(state, getters) {
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(stock => stock.id === stock.id);
+      return {
+        id: stock,
+        quantity: stock.quantity,
+        name: record.name,
+        price: record.price
+      };
+    });
+  },
+  funds(state) {
+    return state.funds;
+  }
+};
